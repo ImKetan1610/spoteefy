1bcd75494fd9ced5c0670cd75bcf180ac3fbdf01
ImKetan1610 <ketan.makode@gmail.com>
2024-03-16 11:33:34 +0530

    .

---
+++ script.js
@@ -0,0 +1,29 @@
+console.log("welcome to spoteefy");
+// Initialize the element 
+let songs = [
+    {
+        songName: "salame Ishq", filePath: "songs/1.mp3", coverPath: "covers/1.jpg"
+    },{
+        songName: "salame Ishq", filePath: "songs/1.mp3", coverPath: "covers/1.jpg"
+    },{
+        songName: "salame Ishq", filePath: "songs/1.mp3", coverPath: "covers/1.jpg"
+    },{
+        songName: "salame Ishq", filePath: "songs/1.mp3", coverPath: "covers/1.jpg"
+    },{
+        songName: "salame Ishq", filePath: "songs/1.mp3", coverPath: "covers/1.jpg"
+    },{
+        songName: "salame Ishq", filePath: "songs/1.mp3", coverPath: "covers/1.jpg"
+    },{
+        songName: "salame Ishq", filePath: "songs/1.mp3", coverPath: "covers/1.jpg"
+    },{
+        songName: "salame Ishq", filePath: "songs/1.mp3", coverPath: "covers/1.jpg"
+    },{
+        songName: "salame Ishq", filePath: "songs/1.mp3", coverPath: "covers/1.jpg"
+    },{
+        songName: "salame Ishq", filePath: "songs/1.mp3", coverPath: "covers/1.jpg"
+    },
+]
+
+
+let audioElement = new Audio("./songs/1.mp3");
+// audioElement.play();
