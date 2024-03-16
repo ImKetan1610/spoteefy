394fc4a2c9d0fb80e7ca83a985957ffce45e7d12
ImKetan1610 <ketan.makode@gmail.com>
2024-03-16 12:04:21 +0530

    added audio element

--- script.js
+++ script.js
@@ -1,29 +1,88 @@
 console.log("welcome to spoteefy");
-// Initialize the element 
-let songs = [
-    {
-        songName: "salame Ishq", filePath: "songs/1.mp3", coverPath: "covers/1.jpg"
-    },{
-        songName: "salame Ishq", filePath: "songs/1.mp3", coverPath: "covers/1.jpg"
-    },{
-        songName: "salame Ishq", filePath: "songs/1.mp3", coverPath: "covers/1.jpg"
-    },{
-        songName: "salame Ishq", filePath: "songs/1.mp3", coverPath: "covers/1.jpg"
-    },{
-        songName: "salame Ishq", filePath: "songs/1.mp3", coverPath: "covers/1.jpg"
-    },{
-        songName: "salame Ishq", filePath: "songs/1.mp3", coverPath: "covers/1.jpg"
-    },{
-        songName: "salame Ishq", filePath: "songs/1.mp3", coverPath: "covers/1.jpg"
-    },{
-        songName: "salame Ishq", filePath: "songs/1.mp3", coverPath: "covers/1.jpg"
-    },{
-        songName: "salame Ishq", filePath: "songs/1.mp3", coverPath: "covers/1.jpg"
-    },{
-        songName: "salame Ishq", filePath: "songs/1.mp3", coverPath: "covers/1.jpg"
-    },
-]
-
-
+// Initialize the element
+let songIndex = 0;
 let audioElement = new Audio("./songs/1.mp3");
+let masterPlay = document.getElementById("masterPlay");
+let myProgressBar = document.getElementById("myProgressBar");
+let gif = document.getElementById("gif");
+
+let songs = [
+  {
+    songName: "salame Ishq",
+    filePath: "songs/1.mp3",
+    coverPath: "covers/1.jpg",
+  },
+  {
+    songName: "salame Ishq",
+    filePath: "songs/1.mp3",
+    coverPath: "covers/1.jpg",
+  },
+  {
+    songName: "salame Ishq",
+    filePath: "songs/1.mp3",
+    coverPath: "covers/1.jpg",
+  },
+  {
+    songName: "salame Ishq",
+    filePath: "songs/1.mp3",
+    coverPath: "covers/1.jpg",
+  },
+  {
+    songName: "salame Ishq",
+    filePath: "songs/1.mp3",
+    coverPath: "covers/1.jpg",
+  },
+  {
+    songName: "salame Ishq",
+    filePath: "songs/1.mp3",
+    coverPath: "covers/1.jpg",
+  },
+  {
+    songName: "salame Ishq",
+    filePath: "songs/1.mp3",
+    coverPath: "covers/1.jpg",
+  },
+  {
+    songName: "salame Ishq",
+    filePath: "songs/1.mp3",
+    coverPath: "covers/1.jpg",
+  },
+  {
+    songName: "salame Ishq",
+    filePath: "songs/1.mp3",
+    coverPath: "covers/1.jpg",
+  },
+  {
+    songName: "salame Ishq",
+    filePath: "songs/1.mp3",
+    coverPath: "covers/1.jpg",
+  },
+];
+
 // audioElement.play();
+
+// handle pause/play click
+masterPlay.addEventListener("click", () => {
+  if (audioElement.paused || audioElement.currentTime <= 0) {
+    audioElement.play();
+    masterPlay.classList.remove("fa-circle-play");
+    masterPlay.classList.add("fa-circle-pause");
+    gif.style.opacity = 1;
+  } else {
+    audioElement.pause();
+    masterPlay.classList.remove("fa-circle-pause");
+    masterPlay.classList.add("fa-circle-play");
+    gif.style.opacity = 0;
+  }
+});
+
+// listen to the events
+audioElement.addEventListener("timeupdate", () => {
+  console.log("timeupdate");
+  //   update seekbar
+  let progress = parseInt(
+    (audioElement.currentTime / audioElement.duration) * 100
+  );
+  console.log(progress);
+  myProgressBar.value = progress;
+});
